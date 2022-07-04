29a91d778c48b9264aa2ba2ca0b1e33c4c0c4464
GabrielRojasBarrera <rojasbarreragabriel@gmail.com>
2022-07-04 06:21:20 -0500

    Practica 2

--- lib/models/models.dart
+++ lib/models/models.dart
@@ -1,1 +1,6 @@
 export 'package:widgetgallery/models/menu_option.dart';
+export 'package:widgetgallery/models/student.dart';
+export 'package:widgetgallery/models/gender.dart';
+export 'package:widgetgallery/widgets/genders_control.dart';
+export 'package:widgetgallery/models/country.dart';
+export 'package:widgetgallery/widgets/dropdown_form.dart';
